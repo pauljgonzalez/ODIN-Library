8f371306c924744dfaddd5c690590eeb8f1d53d1
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-23 11:44:56 -0400

    FEAT: Complete JS for adding books
    Submiting books now adds them to the DOM

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ let title = "";
 let author = "";
 let pages = "";
 let status = "";
-let form = document.getElementById("book-input").elements;
+let counter = 0;
+
 const myLibrary = [];
 
 function Book(title,author,pages,status) {
@@ -10,35 +11,19 @@ function Book(title,author,pages,status) {
   this.author = author;
   this.pages = pages;
   this.status = status;
-  myLibrary.push(this);
   }
 
 document.getElementById("book-input").addEventListener("submit", function(e){
     e.preventDefault();
-    for (let i = 0; i < form.length; i++){
-
-        if(form[i].id ==="title"){
-            title = form[i].value
-            console.log(title)
-        }
-        if(form[i].id ==="author"){
-            author = form[i].value
-            console.log(author)
-        }
-        if(form[i].id ==="pages"){
-            pages = form[i].value
-            console.log(pages)
-        }
-        if(form[i].type === "radio" && form[i].checked){
-            status = form[i].value;
-        } 
-    }
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document.getElementById("pages").value;
+    status = document.querySelector('input[name="bookStatus"]:checked').value;
     document.getElementById("book-input").reset();
-
-    new Book(title,author,pages,status);
+    myLibrary.push(new Book(title,author,pages,status));
+    addDivs();
     reset();
-    console.log(myLibrary);
-    
+    counter ++;
 })
 
 function reset(){
@@ -47,3 +32,31 @@ function reset(){
     pages = "";
     status = "";
 }
+function addDivs(){
+    let bookDisplay = document.querySelector(".book-display");
+    let bookDiv = document.createElement("div");
+    bookDisplay.prepend(bookDiv);
+    let titleDiv = document.createElement("div");
+    titleDiv.className = "title";
+    let authorDiv = document.createElement("div");
+    authorDiv.className = "author";
+    let pagesDiv = document.createElement("div");
+    pagesDiv.className = "pages";
+    let statusDiv = document.createElement("div");
+    statusDiv.className = "status";
+
+    bookDiv.className = "book";
+
+    bookDiv.appendChild(titleDiv)
+    bookDiv.appendChild(authorDiv)
+    bookDiv.appendChild(pagesDiv)
+    bookDiv.appendChild(statusDiv)
+
+    document.querySelector(".book-display .book .title").innerHTML = title;
+    document.querySelector(".book-display .book .author").innerHTML = author;
+    document.querySelector(".book-display .book .pages").innerHTML = pages;
+    document.querySelector(".book-display .book .status").innerHTML = status;
+}
+
+
+
