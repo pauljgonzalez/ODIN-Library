37e6e8b14001636acd84050914702221d1edaa82
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-30 13:17:19 -0400

    Refactored using classes and changed logic to delete books.
    Now looks for the book name and gets the index
    then removes it with that index. Not perfect
    if you have the same book name. Could add a
    unique ID.

--- scripts.js
+++ scripts.js
@@ -1,38 +1,25 @@
-let title = "";
-let author = "";
-let pages = "";
-let status = "";
 let counter = 0;
-
 const myLibrary = [];
-
-function Book(title,author,pages,status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-  }
-
+class Book {
+    constructor(title,author,pages,status){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+}
 document.getElementById("book-input").addEventListener("submit", function(e){
     e.preventDefault();
-    title = document.getElementById("title").value;
-    author = document.getElementById("author").value;
-    pages = document.getElementById("pages").value;
-    status = document.querySelector('input[name="bookStatus"]:checked').value;
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let status = document.querySelector('input[name="bookStatus"]:checked').value;
     document.getElementById("book-input").reset();
     myLibrary.push(new Book(title,author,pages,status));
-    addDivs();
-    reset();
+    addDivs(title);
     counter ++;
 })
-
-function reset(){
-    title = "";
-    author = "";
-    pages = "";
-    status = "";
-}
-function addDivs(){
+function addDivs(looptitle){
     let bookDisplay = document.querySelector(".book-display");
     let bookDiv = document.createElement("div");
     bookDisplay.prepend(bookDiv);
@@ -50,22 +37,25 @@ function addDivs(){
     removeBtn.value = counter;
     removeBtn.onclick = function(){
         this.parentElement.remove();
-        myLibrary.splice(removeBtn.value,1);
+        let index = checkArray(looptitle)
+        myLibrary.splice(index,1);
         counter --;
    }
-   
     bookDiv.appendChild(titleDiv);
     bookDiv.appendChild(authorDiv);
     bookDiv.appendChild(pagesDiv);
     bookDiv.appendChild(statusDiv)
     bookDiv.appendChild(removeBtn);
-
-    
-   
-
-    document.querySelector(".book-display .book .title").innerHTML = title;
-    document.querySelector(".book-display .book .author").innerHTML = "By: "+ author;
-    document.querySelector(".book-display .book .pages").innerHTML = pages +" Pages";
-    document.querySelector(".book-display .book .status").innerHTML = "Status: "+ status;
+    let arrayLength = myLibrary.length - 1;
+    document.querySelector(".book-display .book .title").innerHTML = myLibrary[arrayLength].title;
+    document.querySelector(".book-display .book .author").innerHTML = "By: "+  myLibrary[arrayLength].author;
+    document.querySelector(".book-display .book .pages").innerHTML =  myLibrary[arrayLength].pages +" Pages";
+    document.querySelector(".book-display .book .status").innerHTML = "Status: "+  myLibrary[arrayLength].status;
     document.querySelector(".book-display .book .remove").innerHTML = "Remove Book";
+}
+function checkArray(looptitle){
+    for(let i = 0; i < myLibrary.length; i++){
+        if(myLibrary[i].title === looptitle)
+        return i;
+    }
 }
